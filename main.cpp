ca52d2c4eaa6b0f3394f8cc5fb7ffe038fac53c2
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2025-03-26 16:06:31 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,67 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int i, n, t, j;
+bool scambio;
+cout << "Quanti elementi devono essere generati?" << endl;
+cin>> n;
+int v[n];
+i=0;
+while (i<n)
+{
+    v[i]= rand()n*10 + 1;
+    i=i+1;
+ 
+}
+i=0;
+scambio=true;
+while (i<=n-1 and scambio)
+{
+    j=0;
+    scambio=false;
+    while (j<=(n-2)-i)
+    {
+        if (v[j]>v[j+1])
+        {
+            t=v[j];
+            v[j]=v[j+1];
+            v[j+1]=t;
+            scambio=true;
+        }
+        j=j+1;
+    }
+    i=i+1;
+}
+i=0;
+cout << "Pari" << endl;
+while (i<n)
+{
+    if(v[i]%2==0)
+    {
+        cout << v[i];
+    }
+    i=i+1;
+
+}
+i=0;
+cout << "Dispari" << endl;
+while (i<n)
+{
+    if(v[i]%2==0)
+    {
+        
+    }
+    else 
+    {
+        cout << v[i] << endl;
+    }
+    i=i+1;
+
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
+}
+
+
